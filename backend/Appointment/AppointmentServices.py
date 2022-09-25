749dcc5cb167d7dabe6346c57944e5d1758ed299
trevenue44 <trevenue44.main@gmail.com>
2022-09-25 17:39:00 +0000

    Fix issues concerned with new appointment routes

--- backend/Appointment/AppointmentServices.py
+++ backend/Appointment/AppointmentServices.py
@@ -7,9 +7,11 @@ from Patient.PatientModel import Patient
 
 
 appointment_route = Blueprint("appointment_route", __name__)
-CORS(appointment_route) 
+CORS(appointment_route)
 
 # getting all the appointments in the table
+
+
 @appointment_route.route("/appointments/", methods=["GET"])
 def getAppointments():
     from app import session
@@ -19,7 +21,7 @@ def getAppointments():
             id_doctor = int(request.args.get("id_doctor"))
         except Exception as e:
             return (generate_error_response("Error getting appointment(s)", "id_doctor should be an int.", e), 400)
-        
+
         if request.args.get("status"):
             status = None
             try:
@@ -30,11 +32,14 @@ def getAppointments():
         try:
             # filtering appointments based on status
             if request.args.get("status") and (request.args.get("status").capitalize() in ["Accepted", "Declined", "Pending"]):
-                appointments = session.query(Appointment).filter(Appointment.id_doctor == id_doctor).filter(Appointment.appointment_status == status).all()
+                appointments = session.query(Appointment).filter(
+                    Appointment.id_doctor == id_doctor).filter(Appointment.appointment_status == status).all()
             else:
-                appointments = session.query(Appointment).filter(Appointment.id_doctor == id_doctor).all()
+                appointments = session.query(Appointment).filter(
+                    Appointment.id_doctor == id_doctor).all()
 
-            respones_message = generate_response_message(appointments, "doctor")
+            respones_message = generate_response_message(
+                appointments, "doctor")
             return (respones_message, 200)
         except Exception as e:
             return (generate_error_response("Error getting appointment(s)", "Provided doctor ID might not exist.", e), 404)
@@ -47,12 +52,16 @@ def getAppointments():
         try:
             # filtering appointments based on patient IDs
             if request.args.get("accepted") and (request.args.get("accepted") == "true"):
-                appointments = session.query(Appointment).filter(Appointment.id_patient == id_patient).filter(Appointment.appointment_status == "Accepted").all()
+                appointments = session.query(Appointment).filter(Appointment.id_patient == id_patient).filter(
+                    Appointment.appointment_status == "Accepted").all()
             elif request.args.get("accepted") and (request.args.get("accepted") == "false"):
-                appointments = session.query(Appointment).filter(Appointment.id_patient == id_patient).filter(Appointment.appointment_status != "Accepted").filter(Appointment.appointment_status != "Elapsed").all()
+                appointments = session.query(Appointment).filter(Appointment.id_patient == id_patient).filter(
+                    Appointment.appointment_status != "Accepted").filter(Appointment.appointment_status != "Elapsed").all()
             else:
-                appointments = session.query(Appointment).filter(Appointment.id_patient == id_patient).all()
-            respones_message = generate_response_message(appointments, "patient")
+                appointments = session.query(Appointment).filter(
+                    Appointment.id_patient == id_patient).all()
+            respones_message = generate_response_message(
+                appointments, "patient")
             return (respones_message, 200)
         except Exception as e:
             return (generate_error_response("Error getting appointment(s)", "Provided patient ID might not exist.", e), 404)
@@ -87,30 +96,33 @@ def addAppointment():
                 id_patient=request_content["id_patient"],
                 id_doctor=request_content["id_doctor"]
             )
-            
+
             # checking if the patient and doctors IDs actually exist
             if (
-                session.query(Patient).filter(Patient.id_patient == new_appointment.id_patient)
+                session.query(Patient).filter(
+                    Patient.id_patient == new_appointment.id_patient)
                 and
-                session.query(Doctor).filter(Doctor.id_doctor == new_appointment.id_doctor)
+                session.query(Doctor).filter(
+                    Doctor.id_doctor == new_appointment.id_doctor)
             ):
                 # adding the new appointment to table if doctor and patient do exist
                 session.add(new_appointment)
                 session.commit()
                 # returning a reponse of the newly created appointment
-                reponse_message = generate_response_message([new_appointment], "both")
+                reponse_message = generate_response_message(
+                    [new_appointment], "both")
                 return (reponse_message, 200)
             else:
                 return (generate_error_response("Could not add appointment", "Patient or Doctor does not exist", None), 404)
         except Exception as e:
-            session.rollback()  
+            session.rollback()
             return (generate_error_response("Could not add appointment", "Appointment could not be added", e), 400)
     else:
-        return (generate_error_response("Could not add appointment", "Error: Content-Type Eror", None), 455)        
+        return (generate_error_response("Could not add appointment", "Error: Content-Type Eror", None), 455)
 
 
 # Update appointment by Id
-@appointment_route.route("/appointments/", methods = ['PUT'])
+@appointment_route.route("/appointments/", methods=['PUT'])
 def updateAppointmentById():
     from app import session
     if request.args.get("id_appointment"):
@@ -125,27 +137,27 @@ def updateAppointmentById():
                 appointment = session.query(Appointment).get(id_appointment)
 
                 # updating details with new attributes
-                appointment.appointment_date= req["appointment_date"],
-                appointment.appointment_start_time= req["appointment_start_time"],
-                appointment.appointment_end_time= req["appointment_end_time"],
-                appointment.appointment_reason= req["appointment_reason"],
-                appointment.appointment_status= req["appointment_status"],
+                appointment.appointment_date = req["appointment_date"],
+                appointment.appointment_start_time = req["appointment_start_time"],
+                appointment.appointment_end_time = req["appointment_end_time"],
+                appointment.appointment_reason = req["appointment_reason"],
+                appointment.appointment_status = req["appointment_status"],
                 appointment.appointment_location = req["appointment_location"]
-                appointment.id_patient= req["id_patient"],
-                appointment.id_doctor= req["id_doctor"]
+                appointment.id_patient = req["id_patient"],
+                appointment.id_doctor = req["id_doctor"]
 
-                # Commiting changes to memory 
+                # Commiting changes to memory
                 session.commit()
-                respones_message = generate_response_message([appointment], "both")
+                respones_message = generate_response_message(
+                    [appointment], "both")
                 return (respones_message, 200)
             except Exception as e:
-                session.rollback()  
+                session.rollback()
                 return (generate_error_response("Error updating appointment", "Could not find appointment ID provided", e), 400)
         else:
-            return(generate_error_response("Error updating appointment", "Content-Type error. Expecting payload as JSON", None), 455)
+            return (generate_error_response("Error updating appointment", "Content-Type error. Expecting payload as JSON", None), 455)
     else:
         return (generate_error_response("Error updating appointment", "Expected query paramter 'id_appointment' does not exist.", None), 400)
-        
 
 
 # Deleting appointment by its ID
@@ -164,82 +176,86 @@ def deleteAppointmentById():
             session.commit()
             return (response_message, 200)
         except Exception as e:
-            session.rollback()  
+            session.rollback()
             return (generate_error_response("Error deleting appointment", "id_appointment provided might not exist", None), 400)
     else:
         return (generate_error_response("Error deleting appointment", "Expected query paramter 'id_appointment' does not exist.", None), 400)
 
 
 # Fetch patient's doctors based on patient_id
-@appointment_route.route("/appointments/", methods = ["GET"])
-def fetchAppointmentByPatientId(patientId):
+@appointment_route.route("/appointmentspatient/", methods=["GET"])
+def fetchAppointmentByPatientId():
     from app import session
     try:
         patientId = int(request.args.get("id_patient"))
-        appointments = session.query(Appointment).filter(Appointment.id_patient == patientId).filter(Appointment.appointment_status == 'Accepted' ).all()
+        appointments = session.query(Appointment).filter(Appointment.id_patient == patientId).filter(
+            Appointment.appointment_status == 'Accepted').all()
         returnInfo = []
         for appointment in appointments:
             doctor = session.query(Doctor).get(int(appointment.id_doctor))
             returnInfo.append((
-               {
-                'id_doctor': doctor.id_doctor,
-                'first_name': doctor.first_name,
-                'middle_name': doctor.middle_name,
-                'last_name': doctor.last_name,
-                'user_email': doctor.user_email,
-                'person_image': doctor.person_image,
-                'date_of_birth': doctor.date_of_birth,
-                'house_address': doctor.house_address,
-                'doctor_ratings':doctor.doctor_ratings,
-                'doctor_specialties': doctor.doctor_specialties,
-                'license_number': doctor.license_number,
-                'gender':doctor.gender,
-                'hospital_code':doctor.hospital_code,
-                'hospital_name': doctor.hospital_name,
-                'id_message':doctor.id_message
-               } 
+                {
+                    'id_doctor': doctor.id_doctor,
+                    'first_name': doctor.first_name,
+                    'middle_name': doctor.middle_name,
+                    'last_name': doctor.last_name,
+                    'user_email': doctor.user_email,
+                    'person_image': doctor.person_image,
+                    'date_of_birth': doctor.date_of_birth,
+                    'house_address': doctor.house_address,
+                    'doctor_ratings': doctor.doctor_ratings,
+                    'doctor_specialties': doctor.doctor_specialties,
+                    'license_number': doctor.license_number,
+                    'gender': doctor.gender,
+                    'hospital_code': doctor.hospital_code,
+                    'hospital_name': doctor.hospital_name,
+                    'id_message': doctor.id_message
+                }
             ))
         return ({
             'status': True,
             'msg': returnInfo
-        }),200
+        }), 200
     except Exception as e:
         return ({
-                 'status': False,
-                 'msg':{
-                        "dev_message" : (f"{e}"),
-                        "message":"Could not get appointments"
-                        }
-                }),400
+            'status': False,
+            'msg': {
+                "dev_message": (f"{e}"),
+                "message": "Could not get appointments"
+            }
+        }), 400
 
 # Fetch doctor's patients based on doctor_id
-@appointment_route.route("/appointments/", methods = ["GET"])
-def fetchAppointmentByDoctorId(doctorId):
+
+
+@appointment_route.route("/appointmentsdoctors/", methods=["GET"])
+def fetchAppointmentByDoctorId():
     from app import session
     try:
         doctorId = int(request.args.get("id_doctor"))
-        appointments = session.query(Appointment).filter(Appointment.id_doctor == doctorId).filter(Appointment.appointment_status == 'Accepted' ).all()
+        appointments = session.query(Appointment).filter(Appointment.id_doctor == doctorId).filter(
+            Appointment.appointment_status == 'Accepted').all()
         returnInfo = []
         for appointment in appointments:
             patient = session.query(Patient).get(appointment.id_patient)
             returnInfo.append((
-               {
-                "id_patient": patient.id_patient,
-                "first_name": patient.first_name,
-                "middle_name": patient.middle_name,
-                "last_name": patient.last_name,
-                "user_email": patient.user_email,
-                "person_image": patient.person_image,
-                "date_of_birth":patient.date_of_birth,
-                "phone_number":patient.phone_number,
-                "gender":patient.gender,
-                "id_number": patient.id_number,
-                "id_guardian": patient.id_guardian,
-                "id_doctor": patient.id_doctor,
-                "house_address": patient.house_address,
-                "nationality": patient.nationality,
-                "id_message": patient.id_message
-               } 
+                {
+                    "id_patient": patient.id_patient,
+                    "first_name": patient.first_name,
+                    "middle_name": patient.middle_name,
+                    "last_name": patient.last_name,
+                    "user_email": patient.user_email,
+                    "person_image": patient.person_image,
+                    "date_of_birth": patient.date_of_birth,
+                    "phone_number": patient.phone_number,
+                    "gender": patient.gender,
+                    "id_number": patient.id_number,
+                    "id_guardian": patient.id_guardian,
+                    "id_doctor": patient.id_doctor,
+                    "house_address": patient.house_address,
+                    "nationality": patient.nationality,
+                    "id_message": patient.id_message
+                }
             ))
             # patient = session.query(Patient).get(appointment.id_patient)
             # returnInfo.append((
@@ -252,12 +268,12 @@ def fetchAppointmentByDoctorId(doctorId):
         return ({
             'status': True,
             'msg': returnInfo
-        }),200
+        }), 200
     except Exception as e:
         return ({
-                 'status': False,
-                 'msg':{
-                        "dev_message" : (f"{e}"),
-                        "message":"Could not get appointments"
-                        }
-                }),400
+            'status': False,
+            'msg': {
+                "dev_message": (f"{e}"),
+                "message": "Could not get appointments"
+            }
+        }), 400
--- backend/HealthDetails/HealthDetailsModel.py
+++ backend/HealthDetails/HealthDetailsModel.py
@@ -1,28 +1,29 @@
-from tkinter import CASCADE
 from sqlalchemy import Column, Integer, String, Date, ForeignKey, Float
 from sqlalchemy.orm import relationship
 from Patient.PatientModel import Patient
 from base import Base
 
+
 class HealthDetails(Base):
     __tablename__ = 'HealthDetails'
-    id_healthDetails = Column(Integer, primary_key=True, unique=True, nullable=False, autoincrement=True)
-    id_patient = Column(Integer,ForeignKey(Patient.id_patient,ondelete=CASCADE),nullable = False,unique = True)
-    last_visit = Column("last_visit",Date,nullable=False)
-    blood_group = Column("blood_group", String(7), nullable = False)
-    temperature = Column('temperature',Float)
-    bmi = Column("bmi",Float)
-    blood_pressure = Column("blood_pressure",String(10))
-    respiratory_rate = Column("respiratory_rate",String(10))
-    pulse = Column("pulse",Float)
-    blood_sugar = Column("blood_sugar",String(10))
-    weight = Column("weight",Float)
-    height = Column("height",Float)
+    id_healthDetails = Column(
+        Integer, primary_key=True, unique=True, nullable=False, autoincrement=True)
+    id_patient = Column(Integer, ForeignKey(
+        Patient.id_patient, ondelete='CASCADE'), nullable=False, unique=True)
+    last_visit = Column("last_visit", Date, nullable=False)
+    blood_group = Column("blood_group", String(7), nullable=False)
+    temperature = Column('temperature', Float)
+    bmi = Column("bmi", Float)
+    blood_pressure = Column("blood_pressure", String(10))
+    respiratory_rate = Column("respiratory_rate", String(10))
+    pulse = Column("pulse", Float)
+    blood_sugar = Column("blood_sugar", String(10))
+    weight = Column("weight", Float)
+    height = Column("height", Float)
 
-    patient = relationship("Patient",back_populates = "health_details")
+    patient = relationship("Patient", back_populates="health_details")
 
-
-    def __init__(self, last_visit, blood_group,temperature,blood_pressure, respiratory_rate, pulse, blood_sugar,weight,height,id_patient):
+    def __init__(self, last_visit, blood_group, temperature, blood_pressure, respiratory_rate, pulse, blood_sugar, weight, height, id_patient):
         self.last_visit = last_visit
         self.blood_group = blood_group
         self.temperature = temperature
@@ -30,7 +31,6 @@ class HealthDetails(Base):
         self. respiratory_rate = respiratory_rate
         self. pulse = pulse
         self. blood_sugar = blood_sugar
-        self.id_patient = id_patient 
+        self.id_patient = id_patient
         self.weight = weight
         self.height = height
-    
